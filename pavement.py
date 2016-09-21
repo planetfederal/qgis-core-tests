cd74217b29f04323f59a447ece54f2b8ec57d10a
volaya <volayaf@gmail.com>
2016-09-21 11:50:24 +0200

    added pavement.py

---
+++ pavement.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+#
+# (c) 2016 Boundless, http://boundlessgeo.com
+# This code is licensed under the GPL 2.0 license.
+#
+from cStringIO import StringIO
+import ConfigParser
+from datetime import date, datetime
+import fnmatch
+import os
+from paver.easy import *
+# this pulls in the sphinx target
+from paver.doctools import html
+import xmlrpclib
+import zipfile
+import shutil
+
+options(
+    plugin = Bunch(
+        name = 'coretests',        
+        source_dir = path('coretests'),
+        package_dir = path('.'),
+        tests = ['test', 'tests'],
+        excludes = [
+            'metadata.*',
+            '*.pyc',
+            ".git"
+        ]
+    ),
+
+    sphinx = Bunch(
+        docroot = 'doc',
+        sourcedir = 'source',
+        builddir = 'build'
+    )
+)
+
+
+
+@task
+def setup(options):
+    pass
+
+def _install(folder):
+    '''install plugin to qgis'''
+    plugin_name = options.plugin.name
+    src = path(__file__).dirname() / plugin_name
+    dst = path('~').expanduser() / folder / 'python' / 'plugins' / plugin_name
+    src = src.abspath()
+    dst = dst.abspath()
+    if not hasattr(os, 'symlink'):
+        dst.rmtree()
+        src.copytree(dst)
+    elif not dst.exists():
+        src.symlink(dst)
+
+@task
+def install(options):
+    _install(".qgis2")
+
+@task
+def install-dev(options):
+    _install(".qgis-dev")
+
+@task
+def install3(options):
+    _install(".qgis3")
+
+
+@task
+@cmdopts([
+    ('tests', 't', 'Package tests with plugin'),
+])
+def package(options):
+    '''create package for plugin'''
+    package_file = options.plugin.package_dir / ('%s.zip' % options.plugin.name)
+    with zipfile.ZipFile(package_file, "w", zipfile.ZIP_DEFLATED) as zip:
+        make_zip(zip, options)
+
+
+def make_zip(zip, options):
+    excludes = set(options.plugin.excludes)
+
+    src_dir = options.plugin.source_dir
+    exclude = lambda p: any([fnmatch.fnmatch(p, e) for e in excludes])
+    def filter_excludes(files):
+        if not files: return []
+        # to prevent descending into dirs, modify the list in place
+        for i in xrange(len(files) - 1, -1, -1):
+            f = files[i]
+            if exclude(f):
+                files.remove(f)
+        return files
+
+    for root, dirs, files in os.walk(src_dir):
+        for f in filter_excludes(files):
+            relpath = os.path.relpath(root, '.')
+            zip.write(path(root) / f, path(relpath) / f)
+        filter_excludes(dirs)
+
+
+@task
+def install_devtools():
+    """Install development tools"""
+    try:
+        import pip
+    except:
+        error('FATAL: Unable to import pip, please install it first!')
+        sys.exit(1)
+
+    pip.main(['install', '-r', 'requirements-dev.txt'])
+
+
+@task
+@consume_args
+def pep8(args):
+    """Check code for PEP8 violations"""
+    try:
+        import pep8
+    except:
+        error('pep8 not found! Run "paver install_devtools".')
+        sys.exit(1)
+
+    # Errors to ignore
+    ignore = ['E203', 'E121', 'E122', 'E123', 'E124', 'E125', 'E126', 'E127',
+        'E128', 'E402']
+    styleguide = pep8.StyleGuide(ignore=ignore,
+                                 exclude=['*/ext-libs/*', '*/ext-src/*'],
+                                 repeat=True, max_line_length=79,
+                                 parse_argv=args)
+    styleguide.input_dir(options.plugin.source_dir)
+    info('===== PEP8 SUMMARY =====')
+    styleguide.options.report.print_statistics()
+
+
+@task
+@consume_args
+def autopep8(args):
+    """Format code according to PEP8
+    """
+    try:
+        import autopep8
+    except:
+        error('autopep8 not found! Run "paver install_devtools".')
+        sys.exit(1)
+
+    if any(x not in args for x in ['-i', '--in-place']):
+        args.append('-i')
+
+    args.append('--ignore=E261,E265,E402,E501')
+    args.insert(0, 'dummy')
+
+    cmd_args = autopep8.parse_args(args)
+
+    excludes = ('ext-lib', 'ext-src')
+    for p in options.plugin.source_dir.walk():
+        if any(exclude in p for exclude in excludes):
+            continue
+
+        if p.fnmatch('*.py'):
+            autopep8.fix_file(p, options=cmd_args)
+
+
+@task
+@consume_args
+def pylint(args):
+    """Check code for errors and coding standard violations"""
+    try:
+        from pylint import lint
+    except:
+        error('pylint not found! Run "paver install_devtools".')
+        sys.exit(1)
+
+    if not 'rcfile' in args:
+        args.append('--rcfile=pylintrc')
+
+    args.append(options.plugin.source_dir)
+    lint.Run(args)
